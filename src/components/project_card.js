de660f1a4f2b61775d66ea390c9d8e5adcb0603f
dang lu <dangnlu18@gmail.com>
2020-12-25 14:14:02 -0800

    fixed cards

--- src/components/project_card.js
+++ src/components/project_card.js
@@ -3,11 +3,14 @@ import React from 'react'
 function Card(props){
     return(
         <div className='project-card'>
-            <img src={props.desc.img} alt='conways game of life'/>
-            <div className='card-content'>
-                <h3>{props.desc.title}</h3>
-                <p>{props.desc.content}</p>
+            <div className='card-container'>
+                <img src={props.desc.img} alt='conways game of life'/>
+                <div className='card-content'>
+                    <h3>{props.desc.title}</h3>
+                    <p>{props.desc.content}</p>
+                </div>
             </div>
+
         </div>
     )
 }
